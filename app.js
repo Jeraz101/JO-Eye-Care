8f709a27a20d859fbbe8cb2803b6c40646c3f8dc
Jeff Orazu <jorazu101@gmail.com>
2021-05-20 08:17:56 +0200

    added LOGIN CREATE ACCOUNT and LOGOUT functionality

--- app.js
+++ app.js
@@ -1,26 +1,185 @@
+
+require("dotenv").config()
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const request = require("request");
+const https = require("https");
+const mongoose = require('mongoose')
+const ejs = require("ejs");
+const bcrypt = require("bcrypt"); //
+const saltRounds = 10;
 
 const app = express();
 
-//app.set('view engine', 'ejs'); //basically for ejs
+app.use(express.static("public"));
 
-app.use(express.static("public"));/*when our browser makes a get request to our server.
-The bootstrap and the css disappears or not incoporated in it. to solve this we need to use a special function of express,
-known as app.use(express.static())*/
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 
-app.use(bodyParser.urlencoded({extended:true}));//tells our server to use body-parser for post request
+mongoose.connect("mongodb+srv://admin-jeff:fougnersvei22@cluster0.lzeq8.mongodb.net/subscribersDB", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+})
 
-app.get("/", function(req, res){
 
-  res.sendFile(__dirname + "/index.html")
+const subscribeSchema = new mongoose.Schema({
+
+  email: String
+});
+
+const signUpSchema = new mongoose.Schema({
+  fullName: String,
+  email_address: {
+    type: String,
+    required: [true, "please check your data entry, no email specified!"]
+  },
+
+  password: {
+    type: String,
+    required: [true, "please check your data entry, no password specified!"]
+  }
+
+});
+
+
+
+
+const Subscriber = mongoose.model("Subscriber", subscribeSchema)
+const SignUp = new mongoose.model("SignUp", signUpSchema)
+
+
+
+
+
+
+
+app.get("/", function(req, res) {
+
+  res.render("home")
+});
+
+
+
+
+app.get("/login", function(req, res) {
+
+  res.render("login")
+
+})
+
+app.get("/logout", function(req, res) {
+
+  res.render("login")
+})
+app.get("/signup", function(req, res) {
+  res.render("sign_up")
 })
 
 
 
-app.listen(process.env.PORT || 3000, function(){
+
+
+
+//API
+app.get("/subscribe", function(req, res) {
+
+  Subscriber.find({}, function(err, found_subscibers) {
+    if (err) {
+      res.send(err);
+
+    } else {
+      res.send(found_subscibers);
+    }
+  })
+
+})
+
+
+
+
+
+
+app.post("/subscribe", function(req, res) {
+
+  const subscriber = new Subscriber({
+    email: req.body.usersEmail
+  });
+  subscriber.save(function(err){
+    if(err){
+      console.log(err);
+    }else{
+        res.redirect("/")
+
+    }
+  })
+})
+
+app.post("/signup", function(req, res) {
+
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const signUp = new SignUp({
+      fullName: req.body.fName,
+      email_address: req.body.email,
+      password: hash
+
+    });
+    signUp.save(function(err) {
+      if (!err) {
+        res.render("login");
+      } else {
+        console.log(err);
+
+      }
+    })
+  })
+
+
+})
+
+
+app.post("/login", function(req, res) {
+
+  const email = req.body.userEmail;
+  const password = req.body.userPassword;
+
+
+  SignUp.findOne({email_address: email}, function(err, foundUser) {
+
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) { //using our hased password to login
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result === true) {
+            res.render("home")
+          } else {
+            res.send("<h1>oops!!! sorry, wrong password. Please check your password and try again.</h1>")
+          }
+        });
+      } else {
+        res.send("<h1>ooops! sorry! wrong email, please check your email and login again</h1>")
+      }
+    }
+
+
+    })
+  });
+
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, function() {
 
   console.log("Server started on port 3000");
 });
